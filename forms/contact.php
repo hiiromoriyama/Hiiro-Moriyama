dbc2a61e5eafa066030aa440f958062a0ec479ed
hiiromoriyama <hiiro.m32@gmail.com>
2025-06-06 20:13:42 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -23,15 +23,12 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  'host' => 'smtp.gmail.com',
+  'username' => 'hiiro.m32@gmail.com',
+  'password' => 'qwulmrrlvytjorxu',  // Paste the 16-char app password
+  'port' => '587'
+);
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
